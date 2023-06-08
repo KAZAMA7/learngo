56ffedcadb989f406458d1c354156c2c7a6a6362
Giridhar Konda <giridhar.konda@mambu.com>
2023-06-08 23:30:48 +0200

    strings done

--- 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
+++ 08-numbers-and-strings/02-strings/exercises/08-right-trim-it/main.go
@@ -1,15 +1,9 @@
-// Copyright © 2018 Inanc Gumus
-// Learn Go Programming Course
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// For more tutorials  : https://learngoprogramming.com
-// In-person training  : https://www.linkedin.com/in/inancgumus/
-// Follow me on twitter: https://twitter.com/inancgumus
-
 package main
 
 import (
 	"fmt"
+	"strings"
+	"unicode/utf8"
 )
 
 // ---------------------------------------------------------
@@ -29,9 +23,7 @@ import (
 // ---------------------------------------------------------
 
 func main() {
-	// currently it prints 17
-	// it should print 5
-
 	name := "inanç           "
-	fmt.Println(len(name))
+	name = strings.TrimRight(name, " ")
+	fmt.Println(utf8.RuneCountInString(name))
 }
